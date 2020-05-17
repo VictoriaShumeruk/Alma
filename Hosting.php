d0987f2668388147cb5c12f19f0314bb4c3aae58
VictoriaShumeruk <57069547+VictoriaShumeruk@users.noreply.github.com>
2020-05-17 21:57:51 +0300

    Edit reservation

--- Hosting.php
+++ Hosting.php
@@ -17,67 +17,20 @@ class Hosting extends CI_Controller {
     public function tableMap() {
         if(($this->session->flashdata('key')) != NULL){
             $data = $this->session->flashdata('key');
-            $this->session->keep_flashdata('reDate');
-            $this->load->view('templates/header');
-            $this->load->view('hosting/tableMap', $data);
-            $this->load->view('templates/footer');  
+            $this->session->keep_flashdata('reDate'); 
         }
         else{
          $current_date = date('Y-m-d');
             $data['tables'] = $this->Hosting_model->get_tables($current_date);
+              }
             $this->load->view('templates/header');
             $this->load->view('hosting/tableMap', $data);
-            $this->load->view('templates/footer');  }
-//       if($this->input->post('choose')){
-//            $newDate = date("Y-m-d",strtotime($this->input->post('re_date')));
-//            $data['tables'] = $this->Hosting_model->get_tables($newDate);
-//
-//            $this->load->view('templates/header');
-//            $this->load->view('hosting/tableMap');
-//            $this->load->view('templates/footer');
-//    
-        }
-
-  public function validation($data) {
-        $error = NULL;
-        if (empty($data['fullname'])) {
-            $error['fullname']= 'אנא מלא שם פרטי ומשפחה של הלקוח!';
-        }
-
-        if (empty($data['dine'])) {
-            $error['dine']= 'אנא מלא את מספר הסועדים של ההזמנה!';
-        }
-        
-         if (empty($data['date'])) {
-            $error['date']= 'אנא מלא את התאריך של ההזמנה!';
-        }
-        
-         if (empty($data['time'])) {
-            $error['time']= 'אנא מלא את שעת ההזמנה!';
-        }
-        
-         if ((empty($data['phone'])) && (!preg_match('/^[0-9]{10}+$/', $data['phone']))) {
-            $error['phone']= 'אנא מלא מספר טלפון של הלקוח!';
-        }
-
-        if (!preg_match("/^[a-zA-Zא-ת ]*$/", $data['fullname'])) {
-            $error['name']= 'ניתן להזין אך ורק אותיות ורווחים!';
-        }
-        
-        $this->session->set_flashdata('error', $error);
-    }
-
-    
-    
+            $this->load->view('templates/footer');
+    }    
     
     //form to save new reservation
     public function create() {  
 
-//        if($this->form_validation->run()===false){ 
-//            $this->session->keep_flashdata('error'); 
-//                         
-//        } 
-//        else { 
             $newDate = date("Y-m-d",strtotime($this->input->post('re_date')));
             $newTime = date("H:i", strtotime($this->input->post('re_time')));
             $data = array(
@@ -87,19 +40,100 @@ class Hosting extends CI_Controller {
                 're_date' => $newDate,
                 're_time' => $newTime,
                 'phonenumber' => $this->input->post('phonenumber'),
+                    'fullname' => $this->input->post('fullname'),
             );
-                    
-            $dataclient = array(
-                'fullname' => $this->input->post('fullname'),
-                'phonenumber' => $this->input->post('phonenumber'),
-            );
-                        
-            $this->Hosting_model->form_insert($data, $dataclient);
+//                    
+//            $dataclient = array(
+//                'fullname' => $this->input->post('fullname'),
+//                'phonenumber' => $this->input->post('phonenumber'),
+//            );
+            
+            $error = $this->validation($data);
 
-            echo "הזמנה חדשה התקבלה!";
+        if ($error) {
+            $data['error'] = $this->session->set_flashdata('error','<b><u>שליחת המשמרות נכשלה ! </u></b>'.$error.'');
+//            $ajax_data['error'] = 1; // if error exists then value
+//            $ajax_data['validation_error'] = $error;
             redirect(base_url("/hosting/tableMap"));
+          
+              } else {
+//            $ajax_data['error'] = 0; 
+            $this->Hosting_model->form_insert($data);
+            redirect(base_url("/hosting/tableMap"));
+            }
+//         return json_encode($ajax_data);       
         }
-           
+     
+        public function validation($data) {
+      
+        $error = NULL;
+        if (empty($data['fullname'])) {
+            $error .= '<br> חובה למלא את השם המלא של הלקוח.';
+        }
+
+        if (empty($data['diners'])) {
+            $error .= '<br> חובה להזין  את מספר הסועדים.';
+        }
+        
+         if (empty($data['re_date'])) {
+            $error .= '<br> חובה להזין את תאריך ההזמנה.';
+        }
+        
+         if (empty($data['re_time'])) {
+            $error .= '<br> חובה להזין את שעת ההזמנה.';
+        }
+        
+         if ((empty($data['phonenumber'])) && (!preg_match('/^[0-9]{10}+$/', $data['phonenumber']))) {
+            $error .= '<br> חובה להזין את מספר הטלפןו התקין של הלקוח.';
+        }
+//
+//        if (!preg_match("/^[a-zA-Zא-ת ]*$/", $data['fullname'])) {
+//            $error['name']= 'ניתן להזין אך ורק אותיות ורווחים!';
+//        }
+//        
+        return $error;
+    }
+    
+    public function get_id() {
+       $res_id= $this->input->post('id');
+       $this->session->set_flashdata('reservation_id',$res_id);
+       $this->session->keep_flashdata('reservation_id');
+       redirect(base_url("/hosting/edit_reservation"));
+       
+    }
+    
+    public function edit_reservation() {
   
+        $data['reservation'] = $this->Hosting_model->get_res_by_id($this->session->flashdata('reservation_id'));
+        $this->load->view('templates/header');
+        $this->load->view('Hosting/edit_reservation', $data);
+        $this->load->view('templates/footer');
+    }
+    
+    public function edit() {
+//        $newDate = date("Y-m-d",strtotime($this->input->post('re_date')));
+//        $newTime = date("H:i", strtotime($this->input->post('re_time')));
+//        $data = array(
+//            'num' => $this->input->post('num'),
+//            'location' => $this->input->post('location'),
+//            'diners' => $this->input->post('diners'),
+//            're_date' => $newDate,
+//            're_time' => $newTime,
+//            'phonenumber' => $this->input->post('phonenumber'),
+//        );
+////                    
+//
+//        $dataclient = array(
+//            'fullname' => $this->input->post('fullname'),
+//            'phonenumber' => $this->input->post('phonenumber'),
+//        );
+        echo $id = $this->session->flashdata('reservation_id');
+        $this->Hosting_model->update_res($id);
+        redirect(base_url("/hosting/tableMap"));
+    
+    }
+//    public function getClientNameByNumber(){
+//        
+//    }
 }          
 ?>
--- Hosting_model.php
+++ Hosting_model.php
@@ -6,7 +6,7 @@
         }   
 
         public function get_tables($date){
-            $this->db->join('clients','tables.phonenumber = clients.phonenumber', 'left');
+//            $this->db->join('clients','tables.phonenumber = clients.phonenumber', 'left');
             $this->db->where('re_date',$date);
             $this->db->order_by('re_time', 'ASC');  
             $query=$this->db->get('tables');
@@ -14,10 +14,10 @@
         return $query->result_array();
         }
         
-        public function form_insert($data, $dataclient){
+        public function form_insert($data){
             $this->db->db_debug = FALSE;
              $error = NULL;
-             if (!( ($this->db->insert('tables', $data)) && ($this->db->insert('clients', $dataclient)))) {
+             if (!( $this->db->insert('tables', $data)))  {
                  $error = $this->db->error();
              }
              return $error;
@@ -31,4 +31,44 @@
             $client->setAccessType('offline');
             $client->setPrompt('select_account consent');
         }
+        public function get_res_by_id($data){
+            $this->db->select('*');
+            $this->db->from('tables');
+//             $this->db->join('clients', 'clients.phonenumber = tables.phonenumber', 'left');
+            $this->db->where('id', $data);
+            $query = $this->db->get();
+            $result = $query->row_array();
+            return $result;
+        }
+        public function res_form_input(){
+
+            $newDate = date("Y-m-d",strtotime($this->input->post('re_date')));
+            $newTime = date("H:i", strtotime($this->input->post('re_time')));
+            $data = array(
+            
+                'num' => $this->input->post('num'),
+                'location' => $this->input->post('location'),
+                'diners' => $this->input->post('diners'),
+                're_date' => $newDate,
+                're_time' => $newTime,
+                'phonenumber' => $this->input->post('phonenumber'),
+                'fullname' => $this->input->post('fullname'),  
+                 
+            );
+//        return array($data, $dataclient);
+            return $data;
+        }
+        public function update_res($id){
+            
+           $data = $this->res_form_input();
+//           $this->db->set('$data');
+            $this->db->where('id', $id);
+            $this->db->update('tables', $data);
+
+        }
+        public function update_client($data) {
+            $this->db->set($data);
+           
+        }
+        
 }
