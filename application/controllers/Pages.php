f05f7a5e6cc531bfb5946b9cfbfb37b3fc1758b0
NitzanDagan <62605213+NitzanDagan@users.noreply.github.com>
2020-06-11 17:59:51 +0300

    Add files via upload

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -8,27 +8,73 @@ class Pages extends CI_Controller {
         public function index() { 
         $data['homepage'] = NULL;
         $data['title'] = 'דף הבית' ;
-//        $sess_id = $this->session->userdata('user');
-//        if(empty($sess_id)) //If the user has not logged in  
-//        $data['user'] = null;
-//        else
-//        $data['user'] = $this->session->all_userdata();
 
         $this->load->view('templates/header', $data);
         $this->load->view('homepage', $data);
         $this->load->view('templates/footer');
         }
         
-        
-        public function api(){
-        $data['title'] = 'דף הבית' ;
+        public function music2(){
+        $data['title'] = 'מוזיקה' ;
          
         $this->load->view('templates/header', $data);
-        $this->load->view('pages/api', $data);
+        $this->load->view('pages/music2', $data);
         $this->load->view('templates/footer');
+            
         }
         
         
+        public function music(){
+        $data['title'] = 'מוזיקה' ;
+         
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/music', $data);
+        $this->load->view('templates/footer');
+        }
+        
+//        $params = array('key' => 'df6cd092415291f639da4804ecbdca16');
+//        $this->load->library('deezer-api', $params);
+//        }
+        
+//        $app_id     = "412902";
+//        $app_secret = "df6cd092415291f639da4804ecbdca16";
+//        $my_url     = "http://victoriasu.mtacloud.co.il/Alma/pages/music";
+//
+//        session_start();
+//        $code = $_REQUEST["code"];
+//
+//        if(empty($code)){
+//                $_SESSION['state'] = md5(uniqid(rand(), TRUE)); //CSRF protection
+//
+//                $dialog_url = "https://connect.deezer.com/oauth/auth.php?app_id=".$app_id
+//                        ."&redirect_uri=".urlencode($my_url)."&perms=email,offline_access"
+//                        ."&state=". $_SESSION['state'];
+//
+//                header("Location: ".$dialog_url);
+//                exit;
+//
+//                }
+//
+//        if($_REQUEST['state'] == $_SESSION['state']) {
+//                $token_url = "https://connect.deezer.com/oauth/access_token.php?app_id="
+//                .$app_id."&secret="
+//                .$app_secret."&code=".$code;
+//
+//                $response  = file_get_contents($token_url);
+//                $params    = null;
+//                parse_str($response, $params);
+//                $api_url   = "https://api.deezer.com/user/me?access_token="
+//                                .$params['access_token'];
+//
+//                $user = json_decode(file_get_contents($api_url));
+//               
+//        }
+//    session_encode();
+//    }
+
+        
+        
+
 }
 
 ?>
--- application/controllers/User.php
+++ application/controllers/User.php
@@ -23,27 +23,29 @@ class User extends CI_Controller {
         redirect("user/login");
     }
 
-
     public function auth() {
-
-        $data = array(
+        $login_data = array(
             'id' => $this->input->post('id'),
             'password' => $this->input->post('password'),
         );
-        $check = $this->User_model->auth($data); 
-        
-        if ($check == false) {
-            $data['error'] = "<div class='alert alert-danger'><b>שם משתמש או סיסמא אינם נכונים, נסה שנית  </b></div>";
-            $data['title']='התחברות';
-            $data['user']=NULL;
-            $this->load->view('templates/header', $data);
-            $this->load->view('User/login', $data);
-            $this->load->view('templates/footer');       
-        }
-        else{
-            $this->session->set_userdata($check);
-            redirect("pages/index");
-        }
+        $this->load->view('templates/header');
+        $data['title']='התחברות';
+        if ($this->User_model->check_exist('id', $login_data['id'])) {
+            $check = $this->User_model->auth($login_data); 
+
+            if ($check == false) {
+                $data['error'] = "<div class='alert alert-danger'><b>שם משתמש או סיסמא אינם נכונים, נסה שנית  </b></div>";               
+                $data['user']=NULL;   
+            }
+            else{
+                $this->session->set_userdata($check);
+                redirect("pages/index");
+            }
+        }else{
+            $data['error'] = "<div class='alert alert-danger'><b>המשתמש אינו קיים במערכת</b></div>";
+        } 
+        $this->load->view('User/login', $data);
+        $this->load->view('templates/footer');   
     }   
    
     public function register() {
@@ -59,21 +61,22 @@ class User extends CI_Controller {
             'fullname' => $this->input->post('fullname'),
             'id' => $this->input->post('id'),
             'role' => $this->input->post('role'),
+            'job' => $this->input->post('job'),
             'phone' => $this->input->post('phone'),
             'email' => $this->input->post('email'),
+            'bank_account' => $this->input->post('bank_account'),
+            'birth_date' => $this->input->post('birth_date'),
             'password' => $this->input->post('password'),
         );
-       
+        
         $error = $this->validation($data);
 
         if ($error) {
             $data['error'] = "<div class='alert alert-danger'><b><u>ההרשמה נכשלה </u></b>" . $error . "</div>";
             echo $data['error'];
         } else {
-//            $data['password'] = $data['password'];
-//            unset($data['password']);
-            $this->User_model->save($data);
-           
+            $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT); 
+            $this->User_model->save($data);        
         }
     }
     
@@ -93,16 +96,24 @@ class User extends CI_Controller {
             $error .= '<br>יש למלא את התז';
         }else if(!preg_match('/^[0-9]{9}+$/', $data['id'])) {
             $error .= '<br>תז חייבת לכלול 9 ספרות';
+        }else if ($this->User_model->check_exist('id', $data['id'])) {
+            $error .= '<br>תעודת הזהות הנוכחית כבר רשומה במערכת';
         }
         
-         if (preg_match(('"בחר תפקיד"'), $data['role'])){
+        if (preg_match(('"בחר סטטוס"'), $data['role'])){
+            $error .= '<br>יש לבחור סטטוס';
+        }
+        
+        if (preg_match(('"בחר תפקיד"'), $data['job'])){
             $error .= '<br>יש לבחור תפקיד';
         }
         
          if (empty($data['phone'])) {
             $error .= '<br>יש למלא את מספר הטלפון ';
-        }else if (!preg_match('/^[0-9]{10}+$/', $data['phone'])){
-            $error .= '<br>מספר טלפון אינו תקין';
+        }else if (!preg_match('/^[05]{2}[0-9]{8}$/', $data['phone'])){
+            $error .= '<br> פורמט טלפון אינו תקין - מספר טלפון חייב להתחיל ב"05" ולכלול 10 ספרות סך הכל ';
+        }else if ($this->User_model->check_exist('phone', $data['phone'])) {
+            $error .= '<br>מספר הטלפון הנוכחי כבר רשום במערכת';
         }
         
         if (empty($data['email'])) {
@@ -116,20 +127,6 @@ class User extends CI_Controller {
         return $error;       
     }
     
-    public function userMenu(){
-        $data['title'] = 'פרטי עובדים';
-        $this->load->view('templates/header', $data);
-        $this->load->view('user/userMenu');
-        $this->load->view('templates/footer');
-    }
-    
-    public function editUsers(){
-        $data['title'] = 'פרטי עובדים';
-        $this->load->view('templates/header', $data);
-        $this->load->view('user/editUsers');
-        $this->load->view('templates/footer');
-    }
-    
     public function viewUsers(){
         $data['title'] = 'פרטי עובדים';
         $data['workers'] = $this->User_model->get_users();
@@ -139,12 +136,55 @@ class User extends CI_Controller {
         $this->load->view('templates/footer');
     }
     
-    public function editMyUser(){
-        $data['title'] = 'פרטי עובדים';
-        $this->load->view('templates/header', $data);
-        $this->load->view('user/editMyUser');
-        $this->load->view('templates/footer');
+    public function edit($id){
+        $data['title'] = 'ערוך עובד';
+        
+        $data['workers'] = $this->User_model->get_user_by_id($id);
+        $check = $this->User_model->user_form_input();
+        $error = $this->validationEdit($check);
+        
+        if($error){
+            $this->session->set_flashdata('error','<b><u>עדכון העובד נכשל </u></b>'.$error.'');
+            redirect("user/viewUsers");
+        }
+        else{
+            $this->User_model->update_user($id);
+            $this->session->set_flashdata('success','<b><u>עדכון העובד נשמר בהצלחה </u></b>');
+            redirect("user/viewUsers");
+        }
     }
+    
+    public function validationEdit($data) {
+        $error = NULL;
+               
+        if (!preg_match("/^[a-zA-Zא-ת ]*$/", $data['fullname'])) {
+            $error .= '<br>רק אותיות ורווחים מותרות בשם .';
+        }
+   
+        if(!preg_match('/^[0-9]{9}+$/', $data['id'])) {
+            $error .= '<br>תז חייבת לכלול 9 ספרות';
+        }    
+        
+        if(!preg_match('/^\+?([0-9]{3})\)?[- ]?([0-9]{3})[- ]?([0-9]{6})$/', $data['bank_account'])) {
+            $error .= '<br>פורמט חשבון בנק אינו תקין, חובה להכניס מספר בנק, סניף וחשבון עם קו מפריד בניהם';
+        }    
+        
+        if (!preg_match('/^[05]{2}[0-9]{8}$/', $data['phone'])){
+            $error .= '<br> פורמט טלפון אינו תקין - מספר טלפון חייב להתחיל ב"05" ולכלול 10 ספרות סך הכל ';
+        }
+        
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $error .= '<br>פורמט אימייל אינו תקין';
+        }
+       
+        return $error;       
+    }
+            
+    public function delete_user($id) {
+        $this->User_model->delete_user($id);
+        $this->viewUsers(); 
+    }
+    
     
 }
     
